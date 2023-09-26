95aec8147701dbb06fe03caffe473bfa8bc7221d
DongJinLee <leik24@naver.com>
2023-09-27 07:53:14 +0900

    Time: 1 ms, Memory: 41.1 MB - LeetHub

--- pascals-triangle/pascals-triangle.java
+++ pascals-triangle/pascals-triangle.java
@@ -1,19 +1,19 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> tree = new ArrayList<>();
-            List<Integer> leaf = new ArrayList<>();
-            for (int i = 0; i < numRows; i++) {
-                int prev = 1;
-                leaf.add(1);
-                for (int j = 1; i > 1 && j < i;j++) {
-                    int curr = leaf.get(j);
-                    leaf.set(j, prev + curr);
-                    prev = curr;
-                }
-
-                tree.add(new ArrayList<>(leaf));
+        List<Integer> leaf = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            int prev = 1;
+            leaf.add(1);
+            for (int j = 1; i > 1 && j < i;j++) {
+                int curr = leaf.get(j);
+                leaf.set(j, prev + curr);
+                prev = curr;
             }
 
-            return tree;
+            tree.add(new ArrayList<>(leaf));
+        }
+
+        return tree;
     }
 }
