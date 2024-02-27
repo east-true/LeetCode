5c2d170ff3f03a0fb9001472bb52ac25d776c2f3
DongJinLee <leik24@naver.com>
2024-02-27 14:42:23 +0900

    Time: 1 ms, Memory: 2.1 MB - LeetHub

--- sort-colors/sort-colors.go
+++ sort-colors/sort-colors.go
@@ -1,11 +1,16 @@
 func sortColors(nums []int)  {
     for i := 0; i < len(nums); i++ {
+        minIdx := i
         for j := i+1; j < len(nums); j++ {
-            if nums[i] > nums[j] {
-                k := nums[i]
-                nums[i] = nums[j]
-                nums[j] = k
+            if nums[minIdx] > nums[j] {
+                minIdx = j
             }
+        }
+        
+        if minIdx != i {
+            swap := nums[minIdx]
+            nums[minIdx] = nums[i]
+            nums[i] = swap
         }
     }
 }
