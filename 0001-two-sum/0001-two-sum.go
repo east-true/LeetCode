dd96d3231108c3cf7463a29f10b1673d213c8384
DongJinLee <leik24@naver.com>
2024-08-30 11:58:45 +0900

    Time: 26 ms (25.47%), Space: 3.5 MB (73.26%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.go
@@ -0,0 +1,11 @@
+func twoSum(nums []int, target int) []int {
+    for i := range nums {
+        for j := i+1; j < len(nums); j++ {
+            if nums[i] + nums[j] == target {
+                return []int{i, j}
+            }
+        }
+    }
+
+    return nil
+}
